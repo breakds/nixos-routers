36227a8c7e15c729fd3826d9891e459c7830f20e
Break Yang <breakds@gmail.com>
2021-09-02 15:13:53 -0700

    Make richelieu the entrypoint

--- machines/welderhelper/router.nix
+++ machines/welderhelper/router.nix
@@ -117,7 +117,7 @@ in {
     internalInterfaces = [ vlanLocal ];
     internalIPs = [ "10.77.1.0/24" ];
     forwardPorts = [
-      { sourcePort = 22; destination = "10.77.1.117:22"; loopbackIPs = [ "23.119.127.221" ]; }
+      { sourcePort = 22; destination = "10.77.1.121:22"; loopbackIPs = [ "23.119.127.221" ]; }
       { sourcePort = 80; destination = "10.77.1.121:80"; loopbackIPs = [ "23.119.127.221" ]; }
       { sourcePort = 443; destination = "10.77.1.121:443"; loopbackIPs = [ "23.119.127.221" ]; }
       # For Chia. Let hardstone handle it.
