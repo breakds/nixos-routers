dbf2801f9a87f91faf2d815401b2e360dde36c75
Break Yang <breakds@gmail.com>
2022-10-01 12:01:09 -0700

    Enable ipv6 forwarding

--- machines/welderhelper/router.nix
+++ machines/welderhelper/router.nix
@@ -28,8 +28,8 @@ in {
   boot.kernel.sysctl = {
     "net.ipv4.conf.all.forwarding" = true;
     "net.ipv4.conf.default.forwarding" = true;
-    "net.ipv6.conf.all.forwarding" = false;
-    "net.ipv6.conf.default.forwarding" = false;
+    "net.ipv6.conf.all.forwarding" = true;
+    "net.ipv6.conf.default.forwarding" = true;
   };
 
   # Create 2 separate VLAN devices for the NIC (e.g. eno1). One of the
